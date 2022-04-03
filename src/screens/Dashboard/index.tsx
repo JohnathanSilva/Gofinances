216a9bbda9435868a62e08e29f1190ab3c2a451d
JohnathanSilva <johnathan.castro.silva@hotmail.com>
2022-04-03 00:47:44 -0300

    Using proportion in measure

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,11 +1,21 @@
 import React from "react";
 
-import { Container, Title } from "./styles";
+import { Container, Header, UserWapper, UserInfo, Photo, User, UserGreeting, UserName} from "./styles";
 
 export function Dashboard(){
     return(
         <Container>
-            <Title>Dashboard</Title>
+            <Header>
+                <UserWapper>
+                    <UserInfo>
+                        <Photo source={{uri: 'https://avatars.githubusercontent.com/u/61166807?v=4'}}/>
+                        <User>
+                            <UserGreeting>Olá, </UserGreeting>
+                            <UserName>Johanthan</UserName>
+                        </User>
+                    </UserInfo>
+                </UserWapper>
+            </Header>
         </Container>
     );
 }
