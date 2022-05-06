7c02f0c550661f823eae69e315ce4de31dcebbb9
JohnathanSilva <johnathan.castro.silva@hotmail.com>
2022-05-06 20:12:19 -0300

    Added graphic

--- src/screens/Resume/index.tsx
+++ src/screens/Resume/index.tsx
@@ -1,58 +1,75 @@
 import React, { useEffect, useState } from "react";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import { VictoryPie } from "victory-native";
+import { RFValue } from "react-native-responsive-fontsize";
+
+import { useTheme } from "styled-components";
 
 import { HistoryCard } from "../../components/HistoryCard";
-
-import { Container, Header, Title, Content} from "./styles";
 import { categories } from "../../utils/categories";
+
+import { Container, Header, Title, Content, ChartContainer} from "./styles";
 
 interface TransactionData{
     type: 'positive' | 'negative',
     name: string; 
     amount: string; 
     category: string; 
-    date: string 
+    date: string; 
 }
 
 interface CategoryData{
     key: string;
     name: string;
-    total: string;
+    total: number;
+    totalFormated: string;
     color: string;
+    percent: string;
 }
 
 export function Resume(){
     const [totalByCategories, setTotalByCategories] = useState<CategoryData[]>([]);
+    
+    const theme = useTheme();
 
     async function  loadData() {
         const dataKey = '@gofinances:transactions';
         const response = await AsyncStorage.getItem(dataKey);
         const responseFormatted = response ? JSON.parse(response) : [];
 
-        const expensive = responseFormatted
+        const expensives = responseFormatted
         .filter((expensive: TransactionData) => expensive.type === 'negative');
+
+        const expensiveTotal = expensives.reduce((acumullator: number, expensive:TransactionData) =>{
+            return acumullator + Number(expensive.amount);
+        }, 0);
 
         const totalByCategory:  CategoryData[] = [];
 
         categories.forEach(category => {
             let categorySum =0;
 
-            expensive.forEach((expensive: TransactionData) => {
+            expensives.forEach((expensive: TransactionData) => {
                 if(expensive.category === category.key){
                     categorySum += Number(expensive.amount);
                 }
             });
             
             if(categorySum > 0){
-                const total = categorySum.toLocaleString('pt-BR', {
+                const totalFormated = categorySum.toLocaleString('pt-BR', {
                     style: 'currency',
                     currency: 'BRL'
                 });
+                
+                const percent = `${(categorySum / expensiveTotal * 100).toFixed(0)}%`;
+
                 totalByCategory.push({
                     key: category.key,
                     name: category.name,
                     color: category.color,
-                    total,
+                    total: categorySum,
+                    totalFormated,
+                    percent,
                 });
             }
         });
@@ -68,14 +85,29 @@ export function Resume(){
             <Header>
                 <Title>Resumo por categoria</Title>
             </Header>
-            
+            <ChartContainer>
+                <VictoryPie 
+                    data={totalByCategories}
+                    colorScale={totalByCategories.map(category => category.color)}
+                    style={{
+                        labels: {
+                            fontSize: RFValue(18),
+                            fontWeight: 'bold',
+                            fill: theme.colors.shape,
+                        }
+                    }}
+                    labelRadius={50}
+                    x="percent"
+                    y="total"
+                />
+            </ChartContainer>
             <Content>
                 {
                     totalByCategories.map( item => (  
                         <HistoryCard 
                             key={item.key}
                             title={item.name}
-                            amount={item.total}
+                            amount={item.totalFormated}
                             color={item.color}
                         />
                     ))
